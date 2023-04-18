6fe55c82d5b06cdbb815231944a6626f68433c1d
rohit0879 <124815132+rohit0879@users.noreply.github.com>
2023-04-18 23:47:19 +0530

    Add files via upload

---
+++ 4.Pattern/Pattern3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Pattern3
+{
+    public static void main(String args[]){
+        
+        for(int i=1;i<=4;i++){
+            for(int number=1;number<=i;number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
